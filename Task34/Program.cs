c79e739b05e5559b89bf91a04cdd61a26c8f3f39
Dmitriy <markov068@gmail.com>
2023-06-03 11:48:44 +0300

    Added Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, которая покажет
+// количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    Random rnd = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("");
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+int findEvenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i <= arr.Length - 1; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int numElArr = EnterNumber("Введите количество элементов массива: ");
+int[] arr = CreateArrayRndInt(numElArr, 0, 999);
+PrintArray(arr);
+Console.WriteLine(findEvenNum(arr));
